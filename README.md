18be76ab840a83e6632a02a938a3e5134df350ee
période <pierre.depaz@gmail.com>
2023-01-24 10:52:03 +0100

    morning coffee progress

--- README.md
+++ README.md
@@ -4,9 +4,8 @@
 
 will need individual `parseExpression()`, `parseControlStmt()`, etc. rules with match loops
 
-replace statements by blocks?
-
 ## todo
 - [] environments
+- [] command statements (e.g. `\emph[opt1, opt2]{arg}`)
 - [] comments
 - [] hashmap to store ast
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     let input = "latex_test.tex";
     let src = fs::read_to_string(input).expect("Cannot open file");
 
-    println!("parsing");
+    println!("parsing...");
     let result = LaTeXParser::parse(Rule::document, &src)
                             .expect("Error").next().unwrap();
 
@@ -28,14 +28,26 @@ fn main() {
         match section.as_rule() {
             Rule::section => {
                 for field in section.into_inner() {
-                    println!("  {:?} - {}",field.as_rule(), field.as_str());
-                    for subfield in field.into_inner() {
-                        println!("      {:?} - {}", subfield.as_rule(), subfield.as_str())
+                    println!("  {:?}",field.as_rule());
+                    match field.as_rule() {
+                        Rule::environment_stmt => {
+                            for subfield in field.into_inner() {
+                                println!("      {:?}", subfield.as_rule())
+                            }
+                        },
+                        Rule::command_stmt => {
+                            for subfield in field.into_inner() {
+                                println!("      {:?} - {}", subfield.as_rule(), subfield.as_str())
+                            }
+                        },
+                        Rule::expression => {
+                            for subfield in field.into_inner() {
+                                println!("      {:?} - {}", subfield.as_rule(), subfield.as_str())
+                            }
+                        },
+                        _ => println!("other rule: {:?}", field.as_rule())
                     }
                 }
-            }
-            Rule::control_statement => {
-                println!("direct control statement")
             }
             _ => println!("nothing")
         }
