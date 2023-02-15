79fa3bd7f494af617aaa3edc1368072c14e6d356
période <pierre.depaz@gmail.com>
2023-02-15 16:43:06 +0100

    added static-adapter

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// https://kit.svelte.dev/docs/integrations#preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {}
+	trailingSlash: 'always',
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
