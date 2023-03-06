e77c5a1f97ddf4dc5a8dfb3e62e429a65e9c284f
période <pierre.depaz@gmail.com>
2023-03-06 12:51:09 +0100

    fixed accessibility in header and added zotero metadata

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -3,13 +3,6 @@
   import Header from "./components/interface/Header.svelte";
 </script>
 
-<svelte:head>
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-  />
-</svelte:head>
-
 <Header />
 
 <slot />
