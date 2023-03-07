1a97b33feb36ff5fba965cfa1c8687ffc5b5dee9
période <pierre.depaz@gmail.com>
2023-03-07 18:56:22 +0100

    cosmetic changes

--- web/src/routes/components/Abstract.svelte
+++ web/src/routes/components/Abstract.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div class="my-20">
-    <div class="w-full lg:w-4/12 md:w-6/12 m-auto">
-        <h3 class="mb-4 text-2xl">Abstract</h3>
+    <div class="w-full lg:w-4/12 md:w-6/12 m-auto text-justify">
+        <h3 class="mb-8 text-2xl">Abstract</h3>
         <div class="mb-1 indent-12">{abstract}</div>
     </div>
 </div>
